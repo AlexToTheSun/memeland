583400df1d5d56075a43d46d27e78e2726e9b73b
AlexToTheSun <alexsurf005@gmail.com>
2023-10-31 14:02:17 +0400

    Update filter_tokens.sh

--- filter_tokens.sh
+++ filter_tokens.sh
@@ -1,16 +1,24 @@
 #!/bin/bash
 # vars
-read -p 'Path to needed folder. Example "/root/100accs": ' file_path
-all_acc=${file_path}/accounts.txt
-registred_acc=${file_path}/registered.txt
-emty_acc=${file_path}/empty_attempts.txt
+read -p 'Path to needed folder. Example "/root/100accs/": ' file_path
+all_acc=${file_path}accounts.txt
+registred_acc=${file_path}registered.txt
+emty_acc=${file_path}empty_attempts.txt
 
 #filter registred_acc
-cat $registred_acc | cut -f 1 -d ';' > ${file_path}/filter_registered.txt
-filter_registred_acc=${file_path}/filter_registered.txt
+cat $registred_acc | cut -f 1 -d ';' > ${file_path}filter_registered.txt
+echo "${file_path}filter_registered.txt"
+filter_registred_acc=${file_path}filter_registered.txt
 
-#filter vs 2 files
-grep -vf $all_acc $filter_registred_acc | sort -u > ${file_path}/without_registerd.txt
-grep -vf ${file_path}/without_registerd.txt $emty_acc | sort -u > ${file_path}/without_registerd_and_emty.txt
+# filter private_keys
+cat $registred_acc | cut -f 3 -d ';' > ${file_path}private_keys.txt
+echo "${file_path}private_keys.txt"
+
+#filter 2 files
+grep -vf $filter_registred_acc $all_acc | sort -u > ${file_path}without_registerd.txt
+echo "${file_path}without_registerd.txt"
+
+grep -vf $emty_acc ${file_path}without_registerd.txt | sort -u > ${file_path}without_registerd_and_emty.txt
+echo "${file_path}without_registerd_and_emty.txt"
 
 echo 'Done'
